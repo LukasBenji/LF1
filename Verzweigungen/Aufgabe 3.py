4e2b251a65bdd441e6f995661f84f385e9aff3df
Benjamin217 <benjamin217@schule.bremen.de>
2021-11-16 13:24:41 +0100

    Alkalimetalle sind die Stoffe Litium (Li), Natrium (Na), Kalium(K), Rubidium(Rb) und Ceasium(Cs). Schreibe ein Programm, das Folgendes leistet: Der Benutzer gibt die Formel eines chemischen Elementes an. Anschließend wird gemeldet, ob es sich um ein Alkalimetall handelt oder nicht.

---
+++ Verzweigungen/Aufgabe 3.py
@@ -0,0 +1,7 @@
+Faktor 1 = "400"
+Faktor 2 = "100"
+Faktor 3 = "4"
+Schaltjahr = "0"
+Antwort 1 = "Ja es ist ein Schaltjahr"
+Antwort 2 = "Nein, es ist kein Schaltjahr"
+Frage = input(Ist dieses Jahr ein Schaltjahr?)
